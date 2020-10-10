7fdfe092b0a8b350c37f8b6bf127fd08c1c6128f
billy2anne <kimroque@gmail.com>
2020-10-10 18:20:47 +0000

    exported database schema carts

--- database/dump.sql
+++ database/dump.sql
@@ -16,6 +16,14 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+ALTER TABLE IF EXISTS ONLY public.products DROP CONSTRAINT IF EXISTS products_pkey;
+ALTER TABLE IF EXISTS ONLY public.carts DROP CONSTRAINT IF EXISTS carts_pkey;
+ALTER TABLE IF EXISTS public.products ALTER COLUMN "teaId" DROP DEFAULT;
+ALTER TABLE IF EXISTS public.carts ALTER COLUMN "cartId" DROP DEFAULT;
+DROP SEQUENCE IF EXISTS public."products_teaId_seq";
+DROP TABLE IF EXISTS public.products;
+DROP SEQUENCE IF EXISTS public."carts_cartId_seq";
+DROP TABLE IF EXISTS public.carts;
 DROP EXTENSION IF EXISTS plpgsql;
 DROP SCHEMA IF EXISTS public;
 --
@@ -44,6 +52,142 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: carts; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.carts (
+    "cartId" integer NOT NULL,
+    "createdAt" timestamp(6) with time zone DEFAULT now() NOT NULL
+);
+
+
+--
+-- Name: carts_cartId_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public."carts_cartId_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: carts_cartId_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public."carts_cartId_seq" OWNED BY public.carts."cartId";
+
+
+--
+-- Name: products; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.products (
+    "teaId" integer NOT NULL,
+    name text NOT NULL,
+    price integer NOT NULL,
+    image text NOT NULL,
+    description text NOT NULL
+);
+
+
+--
+-- Name: products_teaId_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public."products_teaId_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: products_teaId_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public."products_teaId_seq" OWNED BY public.products."teaId";
+
+
+--
+-- Name: carts cartId; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.carts ALTER COLUMN "cartId" SET DEFAULT nextval('public."carts_cartId_seq"'::regclass);
+
+
+--
+-- Name: products teaId; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.products ALTER COLUMN "teaId" SET DEFAULT nextval('public."products_teaId_seq"'::regclass);
+
+
+--
+-- Data for Name: carts; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.carts ("cartId", "createdAt") FROM stdin;
+\.
+
+
+--
+-- Data for Name: products; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.products ("teaId", name, price, image, description) FROM stdin;
+1	Green Tea	250	/images/greentea.jpg	Tea is rich in polyphenols, which are natural compounds that have health benefits, such as reducing inflammation and helping to fight cancer.
+2	Jasmine Tea	350	/images/jasminetea.jpg	Tea is floral, sweet, perfumed, fresh, blossomed, dewy, bouquet, fragrant, delicate and subtle.
+3	Black Tea	270	/images/blacktea.jpg	This tea is malty, smoky, brisk, and earthy. It is more oxidized than oolong, yellow, white and green teas. Black tea is generally stronger in flavor than other teas.
+4	Oolong Tea	250	/images/oolongtea.jpg	This tea is oxidized so its flavor is full bodied, floral and toasty. The color of the leaves are golden to brown.
+5	Hibiscus Tea	250	/images/hibiscustea.jpg	This tea is an herbal tea made from the roselle flower. It has many health benefits linked to lowering blood pressure, fighting bacteria and even aid in weight loss.
+6	Raspberry Tea	275	/images/raspberrytea.jpg	This tea is an herbal tea made from raspberry fruit so it has a rich, sweet flavor. It's also rich in nutrients and antioxidants.
+7	White Tea	275	/images/whitetea.jpg	This tea made from Camellia sinensis plant leaves. The flavor is mildly sweet with notes of peach.
+8	Ginger Tea	275	/images/gingertea.jpg	This tea is made from ginger root. It is believed to have antioxidants which increase immunity and decrease stress. It's flavor is aromatic, warm, and spicy.
+9	Citron Honey Tea	250	/images/citronhoneytea.jpg	This tea is made from ginger root. It is believed to have antioxidants which increase immunity and decrease stress. It's flavor is aromatic, warm, and spicy.
+\.
+
+
+--
+-- Name: carts_cartId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public."carts_cartId_seq"', 1, false);
+
+
+--
+-- Name: products_teaId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public."products_teaId_seq"', 1, false);
+
+
+--
+-- Name: carts carts_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.carts
+    ADD CONSTRAINT carts_pkey PRIMARY KEY ("cartId");
+
+
+--
+-- Name: products products_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT products_pkey PRIMARY KEY ("teaId");
 
 
 --
