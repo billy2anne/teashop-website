d146d1852e44d6406f8ec9112c21cc1fef77485f
Kimberly Roque <kimroque@gmail.com>
2020-10-13 19:57:56 -0700

    modified cartSummaryItem component to be renderred on the page

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 
 export default function CartSummaryItem(props) {
-  const totalPrice = this.props.item.price;
+  const totalPrice = props.item.price;
 
   return (
     <>
-      <div>
-        Cart Summary Page
-      </div>
-      <div>
-        {totalPrice}
+      <div className = "cartSummaryItemContainer">
+        <div className="image">
+          <img src={props.item.image} alt="cart image" />
+        </div>
+        <div className="name">
+          {props.item.name}
+        </div>
+        <div>
+          {totalPrice}
+        </div>
+        <div className="description">
+          {props.item.description}
+        </div>
       </div>
     </>
   );
-
 }
--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -2,30 +2,26 @@ import React from 'react';
 import CartSummaryItem from './cart-summary-item';
 export default function CartSummary(props) {
 
-  let totalPrice = 0;
-  for (var i = 0; i < props.cart.length; i++) {
-    totalPrice = (props.cart[i].price / 100) + totalPrice;
-  }
-
   if (props.cart.length === 0) {
     return (
       <div>Your cart is empty</div>
     );
   }
 
+  let totalPrice = 0;
+  for (var i = 0; i < props.cart.length; i++) {
+    totalPrice += (props.cart[i].price / 100);
+  }
   return (
-    <>
-      <div className="cartSummaryContainer">
-        <div onClick={props.view('catalog', {})} className="catalogText">
+    <div className="cartSummaryContainer col-10 align-content-center">
+      <div onClick={() => props.view('catalog', {})} className="catalogText">
           &lt;Back to Menu
-        </div>
-        <h1>Cart Summary</h1>
-        {props.cart.map(item =>
-          <CartSummaryItem item={item} key={item.teaId}/>
-        )}
       </div>
-      <div className="totalPrice">Price: ${(totalPrice).toFixed(2)} </div>
-    </>
+      <h4>Cart Summary</h4>
+      {props.cart.map(item =>
+        <CartSummaryItem item={item} key={item.teaId}/>
+      )}
+      <div className="totalPrice col-10 justify-content-center">Total Price: ${(totalPrice).toFixed(2)} </div>
+    </div>
   );
-
 }
