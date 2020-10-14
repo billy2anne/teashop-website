b7114e085544060d886a942649f47083da429f28
Kimberly Roque <kimroque@gmail.com>
2020-10-13 22:11:47 -0700

    save progress on cart summary frontend

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 
 export default function CartSummaryItem(props) {
-  const totalPrice = props.item.price;
+  const price = props.item.price;
 
   return (
     <>
-      <div className = "cartSummaryItemContainer">
-        <div className="image">
-          <img src={props.item.image} alt="cart image" />
+      <div className= "cartSummaryItemContainer d-flex justify-content-between">
+        <div className="cartItemsimage col-2">
+          <img className="cartItemsimage" src={props.item.image} alt="cart image" />
         </div>
-        <div className="name">
+        <div className="cartItemsname col-3">
           {props.item.name}
         </div>
-        <div>
-          {totalPrice}
-        </div>
-        <div className="description">
-          {props.item.description}
+        <div className="cartdetailsContainer col-5">
+          <div className="cartItemsprice">
+            Price : ${(price / 100).toFixed(2)}
+          </div>
+          <div className="cartItemsdescription">
+            {props.item.description}
+          </div>
+          <button>Place Order</button>
         </div>
       </div>
     </>
