4cbc05f1e580c36aa44099f756c0da11f181db02
Kimberly Roque <kimroque@gmail.com>
2020-10-19 22:12:46 -0700

    modified locations page and included store images

--- client/components/locations.jsx
+++ client/components/locations.jsx
@@ -7,28 +7,46 @@ export default class Locations extends React.Component {
       <>
         <div className="locationContainer col-12 d-flex">
           <div className="row justify-content-center">
-            <div className="location1Container col-8">
+            <div className="location1Container col-9">
               <div className="locationImage1 ">
-                Location image 1
               </div>
               <section>
-                Address goes here
-                Hours goes here
-                Phone Number goes here
-                email goes here
+                ADDRESS:
+                3000 W Green Rd.
+                Culver City, CA 92804
+                <div className="hours">
+                  HOURS:
+                  Sun
+                  8am - 10pm,
+                  Mon-Thu
+                  7am - 10pm,
+                  Fri
+                  7am - 11pm,
+                  Sat
+                  8am - 11pm
+                </div>
               </section>
             </div>
           </div>
           <div className="row justify-content-center">
-            <div className="location2Container col-8">
+            <div className="location2Container col-9">
               <div className="locationImage2">
-                Location image 2
               </div>
               <section>
-                Address goes here
-                Hours goes here
-                Phone Number goes here
-                email goes here
+                ADDRESS:
+                1111 W Apple Rd.
+                Los Angeles, CA 92804
+                <div className="hours">
+                  HOURS:
+                  Sun
+                  8am - 10pm,
+                  Mon-Thu
+                  7am - 10pm,
+                  Fri
+                  7am - 11pm,
+                  Sat
+                  8am - 11pm
+                </div>
               </section>
             </div>
           </div>
@@ -36,28 +54,46 @@ export default class Locations extends React.Component {
         <br/>
         <div className="locationContainer col-12 d-flex">
           <div className="row justify-content-center">
-            <div className="location3Container col-8">
+            <div className="location3Container col-9">
               <div className="locationImage3 ">
-                Location image 3
               </div>
               <section>
-                Address goes here
-                Hours goes here
-                Phone Number goes here
-                email goes here
+                ADDRESS:
+                5178 W Purple Rd.
+                Costa Mesa, CA 92804
+                <div className="hours">
+                  HOURS:
+                  Sun
+                  8am - 10pm,
+                  Mon-Thu
+                  7am - 10pm,
+                  Fri
+                  7am - 11pm,
+                  Sat
+                  8am - 11pm
+                </div>
               </section>
             </div>
           </div>
           <div className="row justify-content-center">
-            <div className="location4Container col-8">
+            <div className="location4Container col-9">
               <div className="locationImage4">
-                Location image 4
               </div>
               <section>
-                Address goes here
-                Hours goes here
-                Phone Number goes here
-                email goes here
+                ADDRESS:
+                2364 W Yellow Rd.
+                Anaheim, CA 92804
+                <div className="hours">
+                  HOURS:
+                  Sun
+                  8am - 10pm,
+                  Mon-Thu
+                  7am - 10pm,
+                  Fri
+                  7am - 11pm,
+                  Sat
+                  8am - 11pm
+                </div>
               </section>
             </div>
           </div>
